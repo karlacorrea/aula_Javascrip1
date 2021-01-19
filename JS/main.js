7a423846d52c55e8401b3346094c0d59fe005de2
karlacorrea <47987828+karlacorrea@users.noreply.github.com>
2021-01-18 23:44:12 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+function clicou(){
+	document.getElementById("agradecimento").innerHTML = <b>"Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+	window.open("https://github.com/karlacorrea/aula_Javascrip1");
+
+}
+
+//var idade = prompt("qual a sua idade?");
+//if (idade >=18){
+//    alert("maior de idade");
+//}else{
+//	alert("menor de idade");
+//};
+//var frutas : [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+//alert ("A casa Caiu!")
